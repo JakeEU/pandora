ae953b0e1caf12a24988e995be05889d6a0f9968
JakeEU <36574990+JakeEU@users.noreply.github.com>
2018-02-19 22:53:37 +0000

    Create commands-p.members.js

---
+++ commands-p.members.js
@@ -0,0 +1,9 @@
+const Disocrd = require ('discord.js')
+module.exports.run = (client, message, args) => {
+        const embed3 = new Discord.RichEmbed()
+        embed3.setColor(0xffa5f1)
+        embed3.setTitle('Our current member list sits at!')
+        embed3.setDescription(`${message.guild.memberCount}`)
+        embed3.setFooter('v1.1')
+        message.channel.send({embed: embed3})
+    };
