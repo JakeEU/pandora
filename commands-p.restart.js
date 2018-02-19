b1fab929412d25047c5735de5e234e6638f64148
JakeEU <36574990+JakeEU@users.noreply.github.com>
2018-02-19 23:07:17 +0000

    Update commands-p.restart.js

--- commands-p.restart.js
+++ commands-p.restart.js
@@ -1,5 +1,5 @@
 const Discord = require ('discord.js')
-exports.run = (client, message, args) => {
+module.exports.run = (client, message, args) => {
   if (message.author.id !== "267006863122497536") return message.channel.send("Erm, you're not my bot owner `jake.` so you can't use this command!").then(msg => { msg.react("❌") });
   message.channel.send("Restarting now! <:pandoraCute:410722647056449547>").then(m => {
       client.destroy().then(() => {
